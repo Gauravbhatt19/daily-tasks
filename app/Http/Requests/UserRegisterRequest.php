866445eaf067b293d3a917af60d064050c88630f
Gauravbhatt19 <gauravbhatt1924@gmail.com>
2021-12-29 14:19:03 +0530

    Register API support

--- app/Http/Requests/UserRegisterRequest.php
+++ app/Http/Requests/UserRegisterRequest.php
@@ -3,6 +3,8 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class UserRegisterRequest extends FormRequest
 {
@@ -45,5 +47,10 @@ class UserRegisterRequest extends FormRequest
             'password.*' => 'Password must contain at least one number, one uppercase, lowercase letter, and at least 8 or more characters',
         ];
         return $messages;
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json($validator->errors(), 422));
     }
 }
